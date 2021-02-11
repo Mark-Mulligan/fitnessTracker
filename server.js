c816dbc87af0bc2834d8773403b530697ba0536a
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-02-11 17:24:06 -0600

    set up express server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, "public/stats.html"));
+});
+
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, "public/exercise.html"));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('server has been started on port 3000');
+})
