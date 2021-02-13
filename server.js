19ce24979127f6ce5ad7d847ecbf814342e36ed9
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-02-13 10:11:40 -0600

    added connection options

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useUnifiedTopology: true 
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 app.get('/', (req, res) => {
